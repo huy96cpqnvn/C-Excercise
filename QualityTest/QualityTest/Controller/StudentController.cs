de45a475965d2cd4f2df6636e220a387d9b7ba0d
huy96cpqnvn <54472256+huy96cpqnvn@users.noreply.github.com>
2020-07-25 18:16:01 +0700

    Edit Display list

--- QualityTest/QualityTest/Controller/StudentController.cs
+++ QualityTest/QualityTest/Controller/StudentController.cs
@@ -11,15 +11,15 @@ namespace QualityTest.Controller
     {
         public void PrintListStudent(List<Student> students)
         {
-            foreach(Student st in students )
+            foreach(var st in students )
             {
-                Console.WriteLine("RollNumber: ", st.RollNumber);
-                Console.WriteLine("FullName: ", st.FullName);
-                Console.WriteLine("Birthday: ", st.Birthday);
-                Console.WriteLine("Email: ", st.Email);
-                Console.WriteLine("Phone: ", st.Phone);
-                Console.WriteLine("CreatedAt: ", st.CreatedAt);
-                Console.WriteLine("Status: ", st.Status);
+                Console.WriteLine("RollNumber: {0}", st.RollNumber);
+                Console.WriteLine("FullName: {0}", st.FullName);
+                Console.WriteLine("Birthday: {0}", st.Birthday);
+                Console.WriteLine("Email: {0}", st.Email);
+                Console.WriteLine("Phone: {0}", st.Phone);
+                Console.WriteLine("CreatedAt: {0}", st.CreatedAt);
+                Console.WriteLine("Status: {0}", st.Status);
             }
         }
 
@@ -31,16 +31,18 @@ namespace QualityTest.Controller
             st.RollNumber = Console.ReadLine();
             Console.WriteLine("Enter FullName:");
             st.FullName = Console.ReadLine();
-            Console.WriteLine("Enter Birthday:");
-            st.Birthday = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter Birthday (dd/mm/yyyy):");
+            var bd = Console.ReadLine();
+            st.Birthday = DateTime.Parse(bd);
             Console.WriteLine("Enter Email:");
             st.Email = Console.ReadLine();
             Console.WriteLine("Enter Phone:");
             st.Phone = Console.ReadLine();
-            Console.WriteLine("Enter CreatedAt:");
-            st.CreatedAt = Console.ReadLine();
+            Console.WriteLine("Enter CreatedAt (dd/mm/yyyy):");
+            var ca = Console.ReadLine();
+            st.CreatedAt = DateTime.Parse(ca);
             Console.WriteLine("Enter Status:");
-            st.Status = Console.ReadLine();
+            Enum.TryParse("Deactive", out StudentStatus Status);
 
             return st;
         }
--- QualityTest/QualityTest/Program.cs
+++ QualityTest/QualityTest/Program.cs
@@ -10,55 +10,39 @@ namespace QualityTest
 {
     class Program
     {
-        public void GenerateMenu()
+        public static void GenerateMenu()
         {
+            List<Student> students = new List<Student>();
+            StudentController stc = new StudentController();
+            int choice;
+            do
+            {
                 Console.WriteLine("----------------Menu-----------------");
                 Console.WriteLine("1. Create New Student");
                 Console.WriteLine("2. View list of Students");
                 Console.WriteLine("3. Exit");
-
-        }
-        static void Main(string[] args)
-        {
-
-            ///DateTime day1 = DateTime.Now;
-            ///DateTime day2 = DateTime.Now.AddDays(-1);
-            ///DateTime.Compare(day1, day2); //Trả về 3 giá trị, nếu day1 > day2 trả về 1, ngược lại là -1, giống nhau trả về 0
-            ///Console.ReadLine();
-
-            ///DateTime join = DateTime.Now.AddDays(-10);
-            ///DateTime twoago = DateTime.Now.AddMonths(-2);
-
-            ///Console.WriteLine((int)StudentStatus.Deactive);
-            ///Console.ReadLine();
-
-            List<Student> listStudent = new List<Student>();
-            int choice = 0;
-            do
-            {
-                listStudent.GenerateMenu();
-
+                Console.WriteLine("-------------------------------------");
                 Console.Write("Enter your choice:");
                 choice = Convert.ToInt32(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
-                        Student n = new Student();
-                        n.CreateStudent();
-                        listStudent.Add(n);
+                        Student st = stc.CreateStudent();
+                        students.Add(st);
                         break;
-
                     case 2:
-                        PrintListStudent(listStudent);
+                        stc.PrintListStudent(students);
                         break;
                     default:
                         break;
                 }
-
             } while (choice != 3);
         }
-
+        static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
     }
-    }
+}
 
