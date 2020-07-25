0d8fafa47cc9ebc7ae830fd01d76e4175e8029eb
huy96cpqnvn <54472256+huy96cpqnvn@users.noreply.github.com>
2020-07-25 17:14:29 +0700

    Update

---
+++ HelloT1904E/QualityTest/QualityTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using QualityTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QualityTest.Controller;
+
+namespace QualityTest
+{
+    class Program
+    {
+        public static void GenerateMenu()
+        {
+            List<Student> students = new List<Student>();
+            StudentController stc = new StudentController();
+            int choice;
+            do
+            {
+                Console.WriteLine("----------------Menu-----------------");
+                Console.WriteLine("1. Create New Student");
+                Console.WriteLine("2. View list of Students");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("-------------------------------------");
+                Console.Write("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Student st = stc.CreateStudent();
+                        students.Add(st);
+                        break;
+                    case 2:
+                        stc.PrintListStudent(students);
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 3);
+        }
+        static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+    }
+}
+
